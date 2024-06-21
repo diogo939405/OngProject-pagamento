2a8ee381f8e1a7061817ba55b0db5f2556d462c9
diogo939405 <diogo.favilla@gmail.ccom>
2024-06-21 16:56:53 -0300

    1

---
+++ MercadoPago.js
@@ -0,0 +1,44 @@
+const { Router } = require('express')
+const mercadopago = require('mercadopago')
+const dotenv = require('dotenv')
+dotenv.config()
+const Mercado_Pago = Router();
+
+mercadopago.configure({
+    access_token: process.env.ACESS_TOKEN || "",
+});
+
+Mercado_Pago.get('/' ,(req,res) =>{
+    res.send('get')
+});
+
+Mercado_Pago.post("/", async (req, res) => {
+    try {
+        const dados = {
+            items: [
+                {
+                    title: "diogo",
+                    unit_price: 200,
+                    currency_id: "BRL",
+                    description: "aqui",
+                    quantity: 1
+                },
+            ],
+            back_urls: {
+                success: "globoesporte.globo.com",
+                failure: "https://www.npmjs.com/package/dotenv",
+            },
+
+            auto_return: "approved",
+        };
+
+        const resposta = await mercadopago.preferences.create(dados);
+        console.log(resposta)
+        res.status(200).json(resposta.response.init_point)
+    } catch (error) {
+        (console.log(error))
+        res.status(500).json(error.message)
+    }
+})
+
+module.exports = Mercado_Pago
---
+++ database/Server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const server = express()
+const cors = require('cors')
+const mercadopago = require('mercadopago')
+const Preference = require('mercadopago')
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+server.use(express.json())
+server.use(cors())
+server.use("Mercado_Pago", mercadopago)
+
+mercadopago.configure({
+    access_token: process.env.ACESS_TOKEN || "",
+});
+
+server.get('/', (req, res) => {
+    res.send('get')
+});
+
+server.post("/dados", async (req, res) => {
+    console.log('req ', req)
+
+    const dado = req.body;
+    // const novosDados = dado.map(e => {
+    //     return {
+    //         title: e.nome,
+    //         unit_price: e.inputValue,
+    //         currency_id: "BRL",
+    //         description: e.descricaoCurta,
+    //         quantity: 1
+    //     };
+    // });
+    try {
+        const dados = {
+            items: [
+                {
+                    title: "diogo",
+                    unit_price: 200,
+                    currency_id: "BRL",
+                    description: "aqui",
+                    quantity: 1
+                },
+            ],
+            back_urls: {
+                success: "globoesporte.globo.com",
+                failure: "https://www.npmjs.com/package/dotenv",
+            },
+
+            auto_return: "approved",
+        };
+        const preference = new Preference(client)
+        const resposta = await mercadopago.preferences.create(dados);
+        console.log(resposta)
+        res.status(200).json(resposta.response.init_point, {
+            id: resposta.id
+        })
+    } catch (error) {
+        (console.log(error))
+        res.status(500).json(error.message)
+    }
+})
+
+
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log('entrou na porta 4000')
+})
+module.exports = server
